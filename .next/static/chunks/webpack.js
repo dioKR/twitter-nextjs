7a68d4b8e4ecbd6988e5e9cc16c19a5541faef3e
dioKR <azss0470@gmail.com>
2024-04-25 09:45:14 +0900

    login page input validation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "37e158395128fe05"; }
+/******/ 		__webpack_require__.h = function() { return "9c092a188fc1591c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/log-in.tsx
+++ pages/log-in.tsx
@@ -1,7 +1,30 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  email: string;
+  password: string;
+}
 
 const Login: NextPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: "onChange",
+  });
+
+  const onValid = (data: LoginForm) => {
+    console.log("im valid bby");
+    console.log(data);
+  };
+
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+
   return (
     <div className="h-screen flex flex-col justify-center">
       <div className="bg-tw-dark-gray rounded-2xl p-7 shadow-2xl">
@@ -14,24 +37,31 @@ const Login: NextPage = () => {
           />
         </h3>
         <h3 className="text-3xl font-bold text-center">Sign in to Twitter</h3>
-        <form>
+        <form onSubmit={handleSubmit(onValid, onInvalid)}>
           <div className="flex flex-col space-y-8 my-10">
             <div className="w-full border-2 border-white rounded-lg bg-white flex gap-2 items-center p-2 focus-within:border-tw-primary">
               <Image src="/arroba.png" width={24} height={24} />
               <input
                 className="w-full bg-transparent outline-0 text-black"
                 id="email"
-                name="email"
                 spellCheck={false}
+                {...register("email", {
+                  required: "Email is required",
+                  validate: {
+                    notEmail: (value) =>
+                      !value.includes("@") || "this mail is not allowed",
+                  },
+                })}
               />
+              {errors.email?.message}
             </div>
             <div className="w-full border-2 border-white rounded-lg bg-white flex gap-2 items-center p-2 focus-within:border-tw-primary">
               <Image src="/padlock.png" width={24} height={24} />
               <input
                 className="w-full bg-transparent outline-0 text-black"
                 id="password"
-                name="password"
                 type="password"
+                {...register("password", { required: "Password is required" })}
               />
             </div>
           </div>
